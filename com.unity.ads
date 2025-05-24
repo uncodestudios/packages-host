c6d16877d6f57c9b49013ae6df82b42e399b9ca3
Jayden <110271631+uncodestudios@users.noreply.github.com>
2025-05-24 17:49:10 -0500

    Add files via upload

---
+++ com.unity.ads
@@ -0,0 +1,28 @@
+{
+  "_id": "com.unity.ads",
+  "name": "com.unity.ads",
+  "description": "Connect your game to the Unity Ads Network for iOS and Android to quickly and effectively monetize your games.\n\nNOTE: Unity has joined forces with ironSource and recommends using Unity LevelPlay for ad mediation (instead of this package). \n\nThis package will still be supported but no longer updated. To install Unity LevelPlay, use the Ads Mediation package (available below) or the LevelPlay plugin-in at https://developers.is.com/ironsource-mobile/unity/unity-plugin/.\n\nSupported platforms:\n- Android\n- iOS\n",
+  "provider": "upm",
+  "versions": {
+    "4.12.0": {
+      "name": "com.unity.ads",
+      "version": "4.12.0",
+      "displayName": "Advertisement Legacy",
+      "description": "Connect your game to the Unity Ads Network for iOS and Android to quickly and effectively monetize your games.\n\nNOTE: Unity has joined forces with ironSource and recommends using Unity LevelPlay for ad mediation (instead of this package). \n\nThis package will still be supported but no longer updated. To install Unity LevelPlay, use the Ads Mediation package (available below) or the LevelPlay plugin-in at https://developers.is.com/ironsource-mobile/unity/unity-plugin/.\n\nSupported platforms:\n- Android\n- iOS\n",
+      "type": "asset",
+      "unity": "2021.3",
+      "keywords": [
+        "ads",
+        "unity"
+      ],
+      "dependencies": {
+        "com.unity.ugui": "1.0.0"
+      },
+      "documentationUrl": "https://docs.unity3d.com/Packages/com.unity.ads@4.12.0/manual/index.html",
+      "dist": {
+        "shasum": "0000000000000000000000000000000000000000",
+        "tarball": "https://uncodestudios.xyz/unity/pkgs/com.unity.ads.tgz"
+      }
+    }
+  }
+}
